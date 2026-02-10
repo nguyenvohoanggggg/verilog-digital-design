205d3102f9d6420ad32aa7bf1c97dc87e648dac9
Nguyen Vo Hoang <nguyenpktdbha@gmail.com>
2026-02-10 17:20:38 +0700

    update

---
+++ tb/full_adder_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ps
+
+module full_adder_tb;
+
+  reg  A_tb, B_tb, Cin_tb;
+  wire Sum_tb, Carry_tb;
+
+  full_adder dut (
+    .A(A_tb),
+    .B(B_tb),
+    .Cin(Cin_tb),
+    .Sum(Sum_tb),
+    .Carry(Carry_tb)
+  );
+
+  initial begin
+    A_tb = 0; B_tb = 0; Cin_tb = 0; #10;
+    A_tb = 0; B_tb = 0; Cin_tb = 1; #10;
+    A_tb = 0; B_tb = 1; Cin_tb = 0; #10;
+    A_tb = 0; B_tb = 1; Cin_tb = 1; #10;
+    A_tb = 1; B_tb = 0; Cin_tb = 0; #10;
+    A_tb = 1; B_tb = 0; Cin_tb = 1; #10;
+    A_tb = 1; B_tb = 1; Cin_tb = 0; #10;
+    A_tb = 1; B_tb = 1; Cin_tb = 1; #10;
+
+    $stop;
+  end
+
+endmodule
+
